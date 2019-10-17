de92e2bdb49b1c888fdcadf01a296cd245f8aa91
Jay <namgirijayvinay@gmail.com>
2019-10-17 09:54:28 +0530

    structure example

---
+++ struct.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+struct person{
+  char name[50];
+  int age;
+  double salary;
+};
+int main()
+{
+  int n;
+  person p1;
+  cout<<"Enter Details of person 1"<<"\nenter name:"<<endl;
+  cin.getline(p1.name,50);
+  cout<<"Enter age:"<<endl;
+  cin>>p1.age;
+  cout<<"Enter salary"<<endl;
+  cin>>p1.salary;
+
+  cout << "Person Details" << endl;
+  cout<<"Peson name: "<<p1.name<<endl;
+  cout<<"Peson age: "<<p1.age<<endl;
+  cout<<"Peson salary: "<<p1.salary<<endl;
+
+}
