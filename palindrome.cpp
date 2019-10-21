707415f4eefc2f17378fbf1b840a3989f88c434b
Jay <namgirijayvinay@gmail.com>
2019-10-21 19:00:33 +0530

    pointers updated int simpler

---
+++ palindrome.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"Enter number:  "<<endl;
+  cin>>a;
+
+}
--- pointers.cpp
+++ pointers.cpp
@@ -3,14 +3,13 @@ using namespace std;
 int main()
 {
   int a=5;
+  int *ptr;// * decodes the address from the varialbe
+  ptr=&a;//& pulls out the address from the variable
+  cout<< "address:  " <<ptr<<endl;
+  cout<< "value " <<*ptr<<endl;
+  int j=25;
   int *p;
-  double *q;
-  float *r;
-  char *s;
-  p=&a;
-  cout<< "Variable value :  " <<a<<endl;
-  cout<< "Address in pointer" <<p<<endl;
-  cout<<"Pointer pointing to value"<<*p<<endl;
-
+  p=&j;
+  cout<<p<<endl;//p is just an address without *
   return 0;
 }
