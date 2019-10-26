80338a4302426832285b8af14666fbd72d9382ce
Jay <namgirijayvinay@gmail.com>
2019-10-27 00:07:48 +0530

    2d

--- array enter ele.cpp
+++ array enter ele.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 int main()
 {
-  int a[20],i,n;
+  int i,n;
+
   cout<<"Enter number of elements: "<<endl;
   cin>>n;
+  int a[n];
   for(i=0; i<n; i++)
   {
-    cout<<"Enter element "<<i<<": "<<endl;
+    cout<<"Enter element "<<i+1<<": "<<endl;
     cin>>a[i];
   }
   cout<<"Array is :\n"<<endl;
   for(i=0; i<n; i++)
   {
-    cout<<a[i]<<"|"<<endl;
+    cout<<a[i]<<"|";
   }
 }
