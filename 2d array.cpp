091771b2862064752147383940f86de9b6fa69b2
Jay <namgirijayvinay@gmail.com>
2019-10-26 23:59:53 +0530

    2d

--- 2d array.cpp
+++ 2d array.cpp
@@ -5,9 +5,10 @@ int main()
   int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
   for (int i=0;i<3;i++)
   {
-    for (int j=1;j<3+1;j++)
+    for (int j=0;j<3;j++)
         {
           cout<<arr[i][j]<<" ";
       }
-  }cout<<endl;
+    cout<<endl;
+  }
 }
